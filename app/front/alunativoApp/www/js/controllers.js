4ce8070c6d845d495b0518bbbf763d4fa574bb9d
Guilherme Ventura <guilhermeventura2@gmail.com>
2016-03-06 08:44:45 -0300

    Arrumado dados de escolas

--- app/front/alunativoApp/www/js/controllers.js
+++ app/front/alunativoApp/www/js/controllers.js
@@ -26,19 +26,47 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('EventsCtrl', function($rootScope, $scope, $state, Events) {
+.controller('EventsCtrl', function($rootScope, $scope, $state, Events, Escolas) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //
+
+    function escolaByEvent(escolas, event) {
+      escolas = escolas || [];
+      var escola = null;
+      for (var i = 0; i < escolas.length; i++) {
+        if (escolas[i].id == event.school_id) {
+          escola = escolas[i];
+        }
+      }
+
+      return escola;
+    }
+
+    function escolaByEvents(escolas, events) {
+      for (var i = 0; i < events.length; i++) {
+        events[i].school = escolaByEvent(escolas, events[i]);
+      }
+    }
+
   $scope.$on('$ionicView.enter', function(e) {
     Events.all().success(function(data) {
       $scope.eventsRecentes = data;
+
+      Escolas.all().success(function(data) {
+        escolaByEvents(data, $scope.eventsRecentes);
+      });
     });
 
     Events.popular().success(function(data) {
       $scope.eventsPopulares = data;
+      console.log(data);
+
+      Escolas.all().success(function(data) {
+        escolaByEvents(data, $scope.eventsPopulares);
+      });
     });
   });
 
@@ -64,12 +92,25 @@ angular.module('starter.controllers', [])
 })
 
 .controller('EventDetailCtrl', function($scope, $stateParams, Events, Escolas) {
+
+  function escolaByEvent(escolas, event) {
+    escolas = escolas || [];
+    var escola = null;
+    for (var i = 0; i < escolas.length; i++) {
+      if (escolas[i].id == event.school_id) {
+        escola = escolas[i];
+      }
+    }
+
+    return escola;
+  }
+
   Events.get($stateParams.eventId).success(function(data) {
     $scope.event = data;
 
-    // Escolas.byEvent($scope.event).success(function(data) {
-    //   $scope.event.school = data;
-    // });
+    Escolas.all().success(function(data) {
+      $scope.event.school = escolaByEvent(data, $scope.event);
+    });
   });
 })
 
